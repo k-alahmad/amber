d1a0bfa379f3ef3588600c539754b404733b890e
moazabdulhalim <moazabdulhalim@gmail.com>
2023-07-04 19:29:54 +0300

    nearby

--- src/components/Layout/PageContainer/index.jsx
+++ src/components/Layout/PageContainer/index.jsx
@@ -3,16 +3,16 @@ import NavBar from "../NavBar";
 import Footer from "../Footer";
 
 const PageLayout = ({ children }) => {
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, []);
-	return (
-		<div className=' '>
-			<NavBar />
-			<div className='min-h-screen'>{children}</div>
-			<Footer />
-		</div>
-	);
+  // useEffect(() => {
+  // 	window.scrollTo(0, 0);
+  // }, []);
+  return (
+    <div className=" ">
+      <NavBar />
+      <div className="min-h-screen">{children}</div>
+      <Footer />
+    </div>
+  );
 };
 
 export default PageLayout;
--- src/pages/home/components/Nearby.jsx
+++ src/pages/home/components/Nearby.jsx
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import icon from "../../../assets/icons/kalifa.svg";
+import heroLeft from "../../../assets/images/heroLeft.png";
+import heroMiddle from "../../../assets/images/heroMiddle.png";
+import { motion } from "framer-motion";
 
 const Nearby = () => {
-	return <div>Nearby Places</div>;
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  let data = [
+    {
+      icon: icon,
+      name: "Plam Juneirah",
+      duration: "25 MINUTES",
+      img: heroLeft,
+    },
+    { icon: icon, name: "some name", duration: "25 MINUTES", img: heroMiddle },
+    { icon: icon, name: "some name", duration: "25 MINUTES", img: heroLeft },
+    { icon: icon, name: "some name", duration: "25 MINUTES", img: heroMiddle },
+    { icon: icon, name: "some name", duration: "25 MINUTES", img: heroLeft },
+    { icon: icon, name: "some name", duration: "25 MINUTES", img: heroMiddle },
+    { icon: icon, name: "some name", duration: "25 MINUTES", img: heroLeft },
+    { icon: icon, name: "some name", duration: "25 MINUTES", img: heroMiddle },
+    { icon: icon, name: "some name", duration: "25 MINUTES", img: heroLeft },
+  ];
+  const GridElement = ({ icon, name, duration, onClick, index }) => {
+    return (
+      <div
+        onClick={onClick}
+        className={`bg-white text-black p-6 h-32 w-40 rounded flex flex-col justify-center items-start space-y-1 cursor-pointer transition-all duration-300 hover:shadow-lg hover:shadow-white/40 hover:bg-white/90 ${
+          index == selectedIndex ? "scale-[1.1] shadow-lg shadow-white/40" : ""
+        }`}
+      >
+        <img src={icon} alt="" className="h-10 w-9" />
+        <p className="text-[16px] font-bold"> {name}</p>
+        <p className=" font-extralight text-[12px]"> {duration} </p>
+      </div>
+    );
+  };
+  return (
+    <div className="bg-[#222222] relative h-[850px] grid grid-cols-12 overflow-hidden">
+      <div className="border-r-[1px] border-white/20" />
+      <div className="border-r-[1px] border-white/20" />
+      <div className="border-r-[1px] border-white/20" />
+      <div className="border-r-[1px] border-white/20" />
+      <div className="absolute  h-full w-full text-white grid grid-cols-12">
+        <div className="pl-16 col-span-4 py-7">
+          <p className="font-bold text-[44px]">EASY ACCESS TO KEY LANDMARKS</p>
+          <p className="text-white/60 font-light text-[18px]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At incidunt
+            culpa nisi dolores, quibusdam itaque laboriosam, placeat in quos
+            fuga soluta suscipit quas, vitae consequuntur molestias aut sit
+            magni dolorum?
+          </p>
+
+          <div className="grid grid-cols-3 gap-7 py-16">
+            {data.map((e, i) => {
+              return (
+                <GridElement
+                  key={i}
+                  icon={e.icon}
+                  name={e.name}
+                  duration={e.duration}
+                  onClick={() => {
+                    setSelectedIndex(i);
+                  }}
+                  index={i}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <motion.div
+          key={selectedIndex}
+          initial={{ opacity: 0, scale: 0.7 }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{ duration: 0.5 }}
+          className="col-start-6 col-span-7 bg-cyan-600"
+        >
+          <img src={data[selectedIndex].img} alt="" className="h-full w-full" />
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default Nearby;
