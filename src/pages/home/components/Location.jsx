6e1f8d13d863810aba854470d4e9fc1ef04e4ad4
moazabdulhalim <moazabdulhalim@gmail.com>
2023-07-05 10:23:38 +0300

    text animation

--- src/pages/home/components/Location.jsx
+++ src/pages/home/components/Location.jsx
@@ -6,7 +6,7 @@ const Element = ({ name, color }) => {
     <div className="flex justify-start items-center">
       <div
         style={{ borderColor: color }}
-        className={`bg-white p-1 border-[1px] rounded-full w-14 h-14`}
+        className={`bg-white p-1 border-[1px] rounded-full w-10 h-10 lg:w-14 lg:h-14`}
       >
         <div
           style={{
@@ -23,23 +23,25 @@ const Element = ({ name, color }) => {
 
 const Location = () => {
   return (
-    <div>
-      <div className="text-9xl text-center bg-transparent text-black my-14 ">
-        <p className="overflow-hidden">
+    <div className="max-md:pt-16">
+      <div className="hidden md:block text-9xl bg-transparent text-black my-14 w-full overflow-hidden">
+        <p className="animate-textCamera relative">
           Site
           <span className="border-text text-transparent ">Location</span>
           MasterPlan
         </p>
       </div>
-      <div className="grid grid-cols-12">
-        <div className="lg:translate-x-16 col-span-4 space-y-8 ">
-          <p className="font-bold text-[44px]">Site Location Masterplan</p>
+      <div className="lg:grid lg:grid-cols-12">
+        <div className="lg:translate-x-16 col-span-4 space-y-4 lg:space-y-8">
+          <p className="font-bold text-[32px] lg:text-[44px]">
+            Site Location Masterplan
+          </p>
           <Element color={"#27AE60"} name={"Apartment Club House"} />
           <Element color={"#D35400"} name={"Lagoon"} />
           <Element color={"#8E44AD"} name={"Low Rise Villa Community"} />
           <Element color={"#2980B9"} name={"Low Rise Villa Community"} />
         </div>
-        <div className="col-span-8">
+        <div className="col-span-8 max-md:pt-8">
           <img src={photo2} alt="" className="h-full w-full" />
         </div>
       </div>
