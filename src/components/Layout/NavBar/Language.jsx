66be9da15a5b8e7a6454f4e4db5028f432a68ed0
Kanaan Alahmad <kanaan92@hotmail.com>
2023-07-06 20:08:50 +0400

    K-nearby-translatation-v.1.2

--- src/components/Layout/NavBar/Language.jsx
+++ src/components/Layout/NavBar/Language.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { RiEnglishInput } from "react-icons/Ri";
 import { IoIosArrowDown } from "react-icons/io";
 import {
 	MdLanguage,
