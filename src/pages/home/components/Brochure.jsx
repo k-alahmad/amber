047a4897638ce814eab6aa527d4eed5de0046f2d
moazabdulhalim <moazabdulhalim@gmail.com>
2023-07-08 17:17:37 +0300

    M-> Brochure

--- src/pages/home/components/Brochure.jsx
+++ src/pages/home/components/Brochure.jsx
@@ -1,30 +1,35 @@
 import React from "react";
-import LazyImage from "../../../components/UI/LazyImage";
 import { useTranslation } from "react-i18next";
 import BrochureImg from "../../../assets/images/Brochure.webp";
 const Brochure = () => {
-	const { i18n, t } = useTranslation();
-	return (
-		<div className='relative  flex p-4 bg-lightBlack mt-40  text-white border-b-2 border-gray-400'>
-			<div
-				className=' max-w-[300px] p-4  justify-center mx-20 items-center'
-				dir={i18n.language == "en" ? "ltr" : "rtl"}
-			>
-				<p className='font-bold tracking-[0.28px] w-[70%] pb-2 text-big md:text-huge '>
-					{t("brochurDownload")}
-				</p>
-				<button className='bg-white rounded-md w-[70%]  p-2 text-lg hover:bg-gray-500 text-lightBlack'>
-					{t("download")}
-				</button>
-			</div>
-			<div className='order-1  '>
-				<img
-					src={BrochureImg}
-					className='absolute w-[150px] h-[200px] md:w-[400px] md:h-[300px] right-0 -translate-y-28'
-				/>
-			</div>
-		</div>
-	);
+  const { i18n, t } = useTranslation();
+  return (
+    <div className="bg-lightBlack mt-44 px-[10%] flex flex-col justify-center items-center">
+      <img
+        src={BrochureImg}
+        className="h-[400px] w-[300px] object-fill col-start-4 col-span-2 -mt-44 lg:hidden"
+      />
+      <div className="text-white lg:grid lg:grid-cols-5 max-lg:py-4">
+        <div
+          className="self-center space-y-2 col-span-2"
+          dir={i18n.language == "en" ? "ltr" : "rtl"}
+        >
+          <p className="font-bold tracking-[0.28px] text-med md:text-big xl:text-huge">
+            {/* {t("brochurDownload")} */}
+            Download your Copy of the Brochure
+          </p>
+          <button className="bg-white rounded-md font-bold p-2 text-lg hover:bg-gray-500 text-lightBlack w-full lg:w-[280px] ">
+            {t("download")}
+          </button>
+        </div>
+        <img
+          src={BrochureImg}
+          className="h-[400px] w-full object-fill col-start-4 col-span-2 -mt-28 hidden lg:block"
+        />
+      </div>
+      <div className="h-[1px] w-full bg-gray-500 relative " />
+    </div>
+  );
 };
 
 export default Brochure;
