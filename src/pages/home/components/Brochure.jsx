d68e48f7c8c1bd4f67e530c79e8816d654c6c810
Kanaan Alahmad <kanaan92@hotmail.com>
2023-07-08 15:54:40 +0400

    K-brochur-translation-v.1.0

--- src/pages/home/components/Brochure.jsx
+++ src/pages/home/components/Brochure.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import LazyImage from "../../../components/UI/LazyImage";
+import { useTranslation } from "react-i18next";
 import BrochureImg from "../../../assets/images/Brochure.webp";
 const Brochure = () => {
+	const { i18n, t } = useTranslation();
 	return (
-		<div className='grid grid-cols-2 bg-lightBlack mt-40 justify-center items-center text-white '>
-			<div>1</div>
-			<div>2</div>
-			{/* <div className=''>
-					<p className='text-xl md:text-huge font-semibold w-full md:w-[70%]'>
-						Download your copy of the brochure
-					</p>
-					<div className='flex justify-center items-center w-[150px] h-[40px] md:w-[300px] md:h-[65px] rounded-md bg-white cursor-pointer hover:bg-white/60'>
-						<p className='text-lightBlack text-lg md:text-big'>Download</p>
-					</div>
-				</div> */}
-			<div className=' '>
-				{/* <LazyImage src={BrochureImg} imgStyle={"w-[500px] -translate-y-44"} /> */}
+		<div className='relative  flex p-4 bg-lightBlack mt-40  text-white border-b-2 border-gray-400'>
+			<div
+				className=' max-w-[300px] p-4  justify-center mx-20 items-center'
+				dir={i18n.language == "en" ? "ltr" : "rtl"}
+			>
+				<p className='font-bold tracking-[0.28px] w-[70%] pb-2 text-big md:text-huge '>
+					{t("brochurDownload")}
+				</p>
+				<button className='bg-white rounded-md w-[70%]  p-2 text-lg hover:bg-gray-500 text-lightBlack'>
+					{t("download")}
+				</button>
+			</div>
+			<div className='order-1  '>
+				<img
+					src={BrochureImg}
+					className='absolute w-[150px] h-[200px] md:w-[400px] md:h-[300px] right-0 -translate-y-28'
+				/>
 			</div>
 		</div>
 	);
--- src/pages/home/components/Register.jsx
+++ src/pages/home/components/Register.jsx
@@ -1,100 +1,105 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import photo1 from "../../../assets/images/photo1.png";
 import {
-  MdLocationOn,
-  MdMail,
-  MdPhone,
-  MdPerson,
-  MdMessage,
+	MdLocationOn,
+	MdMail,
+	MdPhone,
+	MdPerson,
+	MdMessage,
 } from "react-icons/md";
 
 const SocialElement = ({ icon, name }) => {
-  return (
-    <div className="flex items-center">
-      <div className="w-12 h-12 bg-[#222222] text-big flex justify-center items-center">
-        {icon}
-      </div>
-      <p className="font-bold text-med px-4">{name}</p>
-    </div>
-  );
+	return (
+		<div className='flex items-center'>
+			<div className='w-12 h-12 bg-[#222222] text-big flex justify-center items-center'>
+				{icon}
+			</div>
+			<p className='font-bold text-med px-4'>{name}</p>
+		</div>
+	);
 };
 
 const CustomInput = ({ icon, placeholder, name, onChange }) => {
-  return (
-    <div className="border-b-[1px] border-black px-4 py-3 flex">
-      {icon}
-      <input
-        type="text"
-        className="bg-transparent  px-2 w-full outline-none"
-        name={name}
-        onChange={onChange}
-        placeholder={placeholder}
-      />
-    </div>
-  );
+	return (
+		<div className='border-b-[1px] border-black px-4 py-3 flex'>
+			{icon}
+			<input
+				type='text'
+				className='bg-transparent  px-2 w-full outline-none'
+				name={name}
+				onChange={onChange}
+				placeholder={placeholder}
+			/>
+		</div>
+	);
 };
 
 const Register = () => {
-  return (
-    <div className="bg-[#F3F3F3] md:grid md:grid-cols-12">
-      <div className="col-span-8 p-8 px-12 lg:px-16 lg:grid lg:grid-cols-12 pt-20 gap-6">
-        <div className="col-span-6 space-y-6">
-          <p className="font-bold text-huge">Find Your Happy Place</p>
-          <p className="font-light text-small">
-            Find solace in the undisturbed calmness of the surrounding green
-            parks and tree-lined walkways.
-          </p>
-          <div className="space-y-6 pt-8">
-            <SocialElement
-              icon={<MdLocationOn className="text-white" />}
-              name={"Address - Street - District Building No"}
-            />
-            <SocialElement
-              icon={<MdMail className="text-white" />}
-              name={"name@mail.com"}
-            />
-            <SocialElement
-              icon={<MdPhone className="text-white" />}
-              name={"065486264841 - 545631654653"}
-            />
-          </div>
-        </div>
-        <div className="col-span-6 space-y-6 max-lg:pt-8">
-          <CustomInput
-            icon={<MdPerson className="text-black text-big" />}
-            placeholder={"Full Name"}
-          />
-          <CustomInput
-            icon={<MdPhone className="text-black text-big" />}
-            placeholder={"Phone number"}
-          />
-          <CustomInput
-            icon={<MdMail className="text-black text-big" />}
-            placeholder={"Email"}
-          />
-          <div className="border-b-[1px] border-black px-4 py-3 flex">
-            <MdMessage className="text-black text-big" />
-            <textarea
-              type="text"
-              className="border-gray-400 rounded-lg bg-transparent mx-3 px-3 w-full outline-none"
-              rows={8}
-              placeholder="Message"
-            />
-          </div>
-          <button className="bg-[#222222] text-white text-small w-full py-4">
-            Send
-          </button>
-        </div>
-      </div>
-      <div className="col-span-4 ">
-        <img
-          src={photo1}
-          alt=""
-          className="w-full max-md:h-[300px]  md:h-full object-cover"
-        />
-      </div>
-    </div>
-  );
+	const { i18n, t } = useTranslation();
+	return (
+		<div className='bg-[#F3F3F3] md:grid md:grid-cols-12'>
+			<div className='col-span-8 p-8 px-12 lg:px-16 lg:grid lg:grid-cols-12 pt-20 gap-6'>
+				<div
+					className='col-span-6 space-y-6'
+					dir={i18n.language == "en" ? "ltr" : "rtl"}
+				>
+					<p className='font-bold text-huge'>{t("slogan")}</p>
+					<p className='font-light text-small'>{t("formSubTitle")}</p>
+					<div className='space-y-6 pt-8'>
+						<SocialElement
+							icon={<MdLocationOn className='text-white' />}
+							name={"Business Bay - Clover Bay Tower - Ava Real Estate"}
+						/>
+						{/* <SocialElement
+							icon={<MdMail className='text-white' />}
+							name={"www.avarealestate.ae"}
+						/> */}
+						{/* <SocialElement
+							icon={<MdPhone className='text-white' />}
+							name={"065486264841 - 545631654653"}
+						/> */}
+					</div>
+				</div>
+				<div
+					className='col-span-6 space-y-6 max-lg:pt-8'
+					dir={i18n.language == "en" ? "ltr" : "rtl"}
+				>
+					<CustomInput
+						icon={<MdPerson className='text-black text-big' />}
+						placeholder={t("formFullName")}
+					/>
+					<CustomInput
+						icon={<MdPhone className='text-black text-big' />}
+						placeholder={t("formPhoneNumber")}
+					/>
+					<CustomInput
+						icon={<MdMail className='text-black text-big' />}
+						placeholder={t("formEmail")}
+					/>
+					{/* <div className='border-b-[1px] border-black px-4 py-3 flex'>
+						<MdMessage className='text-black text-big' />
+						<textarea
+							type='text'
+							className='border-gray-400 rounded-lg bg-transparent mx-3 px-3 w-full outline-none'
+							rows={8}
+							placeholder='Message'
+						/>
+					</div> */}
+					<button className='bg-[#222222] text-white text-small w-full py-4'>
+						{t("register")}
+					</button>
+				</div>
+			</div>
+			<div className='col-span-4 '>
+				<img
+					src={photo1}
+					alt=''
+					className='w-full max-md:h-[300px]  md:h-full object-cover'
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Register;
