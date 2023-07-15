bd89b4c1561c89179d656d0248b00e0d1be02493
Kanaan Alahmad <kanaan92@hotmail.com>
2023-07-15 14:17:55 +0400

    K-floorplan-divider-v1.2

--- src/pages/home/components/SectionDivider.jsx
+++ src/pages/home/components/SectionDivider.jsx
@@ -4,7 +4,7 @@ import dubai from "../../../assets/images/dubai.webp";
 const SectionDivider = () => {
 	return (
 		<div
-			className='relative w-full h-screen justify-center items-center bg-center bg-fixed bg-blue-600'
+			className='relative w-full h-screen justify-center items-center bg-center bg-fixed '
 			style={{
 				backgroundImage: `url(${dubai})`,
 				backgroundRepeat: "no-repeat",
