1535e3766284a4fcc781fbafcaa5d012cbfe5b28
Kanaan Alahmad <kanaan92@hotmail.com>
2023-11-07 14:57:12 +0400

    K-Fixes

--- src/pages/home/components/Register.jsx
+++ src/pages/home/components/Register.jsx
@@ -2,20 +2,21 @@ import React, { useState, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import photo1 from "../../../assets/images/register.webp";
 import {
-  MdLocationOn,
-  MdMail,
-  MdPhone,
-  MdPerson,
-  //   MdMessage
+	MdLocationOn,
+	MdMail,
+	MdPhone,
+	MdPerson,
+	//   MdMessage
 } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  hideModal,
-  register,
-  counterIsFull,
-  selectDownloadState,
-  selectRegisterState,
-  selectCounterState,
+	hideModal,
+	register,
+	counterIsFull,
+	selectDownloadState,
+	selectRegisterState,
+	selectCounterState,
+	selectListRegisterState,
 } from "../../../redux/modal.slice";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/bootstrap.css";
@@ -23,238 +24,238 @@ import emailjs from "@emailjs/browser";
 import Brochure from "../../../assets/pdf/330.pdf";
 import { useNavigate } from "react-router-dom";
 const SocialElement = ({ icon, name }) => {
-  return (
-    <div className="flex items-center">
-      <div className="w-12 h-12 bg-[#222222] text-big flex justify-center items-center">
-        {icon}
-      </div>
-      <p className="font-bold text-base px-4 ">{name}</p>
-    </div>
-  );
+	return (
+		<div className='flex items-center'>
+			<div className='w-12 h-12 bg-[#222222] text-big flex justify-center items-center'>
+				{icon}
+			</div>
+			<p className='font-bold text-base px-4 '>{name}</p>
+		</div>
+	);
 };
 
 const CustomInput = ({
-  icon,
-  placeholder,
-  name,
-  id,
-  value,
-  onChange,
-  error,
+	icon,
+	placeholder,
+	name,
+	id,
+	value,
+	onChange,
+	error,
 }) => {
-  return (
-    <>
-      <div className="border-b-[1px] text-white border-white px-4 py-3 flex">
-        {icon}
-        <input
-          type="text"
-          className="bg-transparent  px-2 w-full outline-none"
-          name={name}
-          onChange={onChange}
-          placeholder={placeholder}
-          id={id}
-          value={value}
-        />
-      </div>
-      {error && (
-        <p className="text-smaller text-red-700">Please Enter A Valid Email</p>
-      )}
-    </>
-  );
+	return (
+		<>
+			<div className='border-b-[1px] text-white border-white px-4 py-3 flex'>
+				{icon}
+				<input
+					type='text'
+					className='bg-transparent  px-2 w-full outline-none'
+					name={name}
+					onChange={onChange}
+					placeholder={placeholder}
+					id={id}
+					value={value}
+				/>
+			</div>
+			{error && (
+				<p className='text-smaller text-red-700'>Please Enter A Valid Email</p>
+			)}
+		</>
+	);
 };
 function Register({ modal }) {
-  const dispatch = useDispatch();
-  const { i18n, t } = useTranslation();
-  const [email, setEmail] = useState("");
-  const [fullName, setFullName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [emailError, setEmailError] = React.useState("");
-  const listRegister = useSelector(selectListRegisterState);
+	const dispatch = useDispatch();
+	const { i18n, t } = useTranslation();
+	const [email, setEmail] = useState("");
+	const [fullName, setFullName] = useState("");
+	const [phone, setPhone] = useState("");
+	const [emailError, setEmailError] = React.useState("");
+	const listRegister = useSelector(selectListRegisterState);
 
-  const downloadState = useSelector(selectDownloadState);
-  const registerState = useSelector(selectRegisterState);
-  const counterState = useSelector(selectCounterState);
-  const form = useRef();
-  const sendEmail = (e) => {
-    e.preventDefault();
-    emailjs
-      .sendForm(
-        "service_5wdnu6j",
-        "template_slqqcpm",
-        form.current,
-        "sxh5TJan60LQqD6Sw"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          // window.location.reload(false);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+	const downloadState = useSelector(selectDownloadState);
+	const registerState = useSelector(selectRegisterState);
+	const counterState = useSelector(selectCounterState);
+	const form = useRef();
+	const sendEmail = (e) => {
+		e.preventDefault();
+		emailjs
+			.sendForm(
+				"service_5wdnu6j",
+				"template_slqqcpm",
+				form.current,
+				"sxh5TJan60LQqD6Sw",
+			)
+			.then(
+				(result) => {
+					console.log(result.text);
+					// window.location.reload(false);
+				},
+				(error) => {
+					console.log(error.text);
+				},
+			);
+	};
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    let formData = new FormData(form.current);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		let formData = new FormData(form.current);
 
-    try {
-      let sameEmail = listRegister.find((element) => {
-        return element.email == form.current.email.value;
-      });
-      let samePhone = listRegister.find((element) => {
-        return element.phone == form.current.phone.value;
-      });
+		try {
+			let sameEmail = listRegister.find((element) => {
+				return element.email == form.current.email.value;
+			});
+			let samePhone = listRegister.find((element) => {
+				return element.phone == form.current.phone.value;
+			});
 
-      if (!sameEmail && !samePhone) {
-        const response = await fetch(
-          "https://hooks.zapier.com/hooks/catch/12792925/3mwu9z9/",
-          {
-            method: "POST",
-            body: formData,
-            "Content-Type": "multipart/form-data",
-          }
-        );
-        const result = response.json();
-        console.log("Success:", result);
-        sendEmail(e);
-        if (downloadState && registerState) {
-          let alink = document.createElement("a");
-          alink.href = Brochure;
-          alink.download = "BrochurePdf.pdf";
-          alink.click();
-        }
-        dispatch(
-          register({
-            email: form.current.email.value,
-            phoneNo: form.current.phone.value,
-          })
-        );
-        dispatch(counterIsFull());
-        dispatch(hideModal());
-        navigate("/thankyou");
-      } else {
-        alert("We Already Have a registration with this data");
-      }
-    } catch (error) {
-      console.error("Error here:", error);
-    }
-  };
+			if (!sameEmail && !samePhone) {
+				const response = await fetch(
+					"https://hooks.zapier.com/hooks/catch/12792925/3mwu9z9/",
+					{
+						method: "POST",
+						body: formData,
+						"Content-Type": "multipart/form-data",
+					},
+				);
+				const result = response.json();
+				console.log("Success:", result);
+				sendEmail(e);
+				if (downloadState && registerState) {
+					let alink = document.createElement("a");
+					alink.href = Brochure;
+					alink.download = "BrochurePdf.pdf";
+					alink.click();
+				}
+				dispatch(
+					register({
+						email: form.current.email.value,
+						phoneNo: form.current.phone.value,
+					}),
+				);
+				dispatch(counterIsFull());
+				dispatch(hideModal());
+				navigate("/thankyou");
+			} else {
+				alert("We Already Have a registration with this data");
+			}
+		} catch (error) {
+			console.error("Error here:", error);
+		}
+	};
 
-  //   const disableOnClick = (event) => (event.currentTarget.disabled = true);
-  function handleChangeEmail(e) {
-    setEmailError("");
-    setEmail(e.target.value);
-    let EmailRegExp;
-    EmailRegExp = new RegExp(
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    ).test(e.target.value);
+	//   const disableOnClick = (event) => (event.currentTarget.disabled = true);
+	function handleChangeEmail(e) {
+		setEmailError("");
+		setEmail(e.target.value);
+		let EmailRegExp;
+		EmailRegExp = new RegExp(
+			/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+		).test(e.target.value);
 
-    if (!EmailRegExp) {
-      setEmailError("Email Is Not Valid");
-    }
-  }
-  return (
-    <div
-      dir={i18n.language == "en" ? "ltr" : "rtl"}
-      className={`md:grid md:grid-cols-12  bg-transparent`}
-    >
-      <div className="col-span-8 p-8 lg:px-16 xl:grid xl:grid-cols-12 gap-6">
-        {downloadState && !registerState && modal && (
-          <div className="col-span-12 text-big lg:text-bigger font-bold text-red-800 flex justify-center items-center">
-            {t("brochurMSG")}
-          </div>
-        )}
-        {counterState && !registerState && !downloadState && modal && (
-          <div className="col-span-12 text-big lg:text-bigger font-bold text-red-800 flex justify-center items-center">
-            {/* {t("brochurMSG")} */}
-            Please Register to view all Rooms
-          </div>
-        )}
-        <div className="col-span-6 space-y-12 flex flex-col justify-center items-stretch">
-          <p className="font-bold text-bigger xl:text-huge ">{t("slogan")}</p>
-          <p className="font-light text-small">{t("formSubTitle")}</p>
-        </div>
-        <form
-          ref={form}
-          onSubmit={handleSubmit}
-          className="col-span-6 lg:row-span-2 space-y-12 max-lg:pt-8 flex flex-col justify-center items-stretch"
-        >
-          <CustomInput
-            icon={<MdPerson className="text-white text-big" />}
-            placeholder={t("formFullName")}
-            name="fullName"
-            id="fullName"
-            value={fullName}
-            onChange={(event) => setFullName(event.target.value)}
-          />
-          <input
-            type="text"
-            className="hidden"
-            name={"project"}
-            value="Amber Project"
-          />
+		if (!EmailRegExp) {
+			setEmailError("Email Is Not Valid");
+		}
+	}
+	return (
+		<div
+			dir={i18n.language == "en" ? "ltr" : "rtl"}
+			className={`md:grid md:grid-cols-12  bg-transparent`}
+		>
+			<div className='col-span-8 p-8 lg:px-16 xl:grid xl:grid-cols-12 gap-6'>
+				{downloadState && !registerState && modal && (
+					<div className='col-span-12 text-big lg:text-bigger font-bold text-red-800 flex justify-center items-center'>
+						{t("brochurMSG")}
+					</div>
+				)}
+				{counterState && !registerState && !downloadState && modal && (
+					<div className='col-span-12 text-big lg:text-bigger font-bold text-red-800 flex justify-center items-center'>
+						{/* {t("brochurMSG")} */}
+						Please Register to view all Rooms
+					</div>
+				)}
+				<div className='col-span-6 space-y-12 flex flex-col justify-center items-stretch'>
+					<p className='font-bold text-bigger xl:text-huge '>{t("slogan")}</p>
+					<p className='font-light text-small'>{t("formSubTitle")}</p>
+				</div>
+				<form
+					ref={form}
+					onSubmit={handleSubmit}
+					className='col-span-6 lg:row-span-2 space-y-12 max-lg:pt-8 flex flex-col justify-center items-stretch'
+				>
+					<CustomInput
+						icon={<MdPerson className='text-white text-big' />}
+						placeholder={t("formFullName")}
+						name='fullName'
+						id='fullName'
+						value={fullName}
+						onChange={(event) => setFullName(event.target.value)}
+					/>
+					<input
+						type='text'
+						className='hidden'
+						name={"project"}
+						value='Amber Project'
+					/>
 
-          <CustomInput
-            icon={<MdMail className="text-white text-big" />}
-            placeholder={t("formEmail")}
-            name="email"
-            id="email"
-            value={email}
-            onChange={handleChangeEmail}
-            error={Boolean(emailError)}
-          />
-          <PhoneInput
-            country={"ae"}
-            placeholder={t("formPhoneNumber")}
-            enableSearch={true}
-            inputProps={{
-              name: "phone",
-              id: "phone",
-              required: true,
-            }}
-            onChange={setPhone}
-            containerClass="!border-b-[1px] border-white px-1 flex "
-            inputClass={`!bg-transparent !w-full !text-white !text-lg !h-full !border-none  ${
-              i18n.language == "en" ? "px-0" : "mx-10"
-            } !outline-none`}
-            buttonClass={`!border-none !text-lg `}
-            buttonStyle={{ direction: "ltr" }}
-            inputStyle={{
-              direction: "ltr",
-            }}
-          />
-          <button
-            className="bg-[#0047ab] rounded-md text-white text-small w-full py-4 hover:bg-[#0047ab77] disabled:bg-[#36454f] "
-            disabled={
-              email.replace(/ /g, "") == "" ||
-              Boolean(emailError) ||
-              fullName.replace(/ /g, "") == "" ||
-              phone.length < 12
-            }
-          >
-            {t("register")}
-          </button>
-        </form>
-        <div className="space-y-6 pt-8 lg:pt-2 lg:col-span-6">
-          <SocialElement
-            icon={<MdLocationOn className="text-white" />}
-            name={t("address")}
-          />
-        </div>
-      </div>
-      <div className="col-span-4">
-        <img
-          src={photo1}
-          alt="Register Image"
-          className="w-full max-md:h-[300px] md:h-[600px] object-cover"
-        />
-      </div>
-    </div>
-  );
+					<CustomInput
+						icon={<MdMail className='text-white text-big' />}
+						placeholder={t("formEmail")}
+						name='email'
+						id='email'
+						value={email}
+						onChange={handleChangeEmail}
+						error={Boolean(emailError)}
+					/>
+					<PhoneInput
+						country={"ae"}
+						placeholder={t("formPhoneNumber")}
+						enableSearch={true}
+						inputProps={{
+							name: "phone",
+							id: "phone",
+							required: true,
+						}}
+						onChange={setPhone}
+						containerClass='!border-b-[1px] border-white px-1 flex '
+						inputClass={`!bg-transparent !w-full !text-white !text-lg !h-full !border-none  ${
+							i18n.language == "en" ? "px-0" : "mx-10"
+						} !outline-none`}
+						buttonClass={`!border-none !text-lg `}
+						buttonStyle={{ direction: "ltr" }}
+						inputStyle={{
+							direction: "ltr",
+						}}
+					/>
+					<button
+						className='bg-[#0047ab] rounded-md text-white text-small w-full py-4 hover:bg-[#0047ab77] disabled:bg-[#36454f] '
+						disabled={
+							email.replace(/ /g, "") == "" ||
+							Boolean(emailError) ||
+							fullName.replace(/ /g, "") == "" ||
+							phone.length < 12
+						}
+					>
+						{t("register")}
+					</button>
+				</form>
+				<div className='space-y-6 pt-8 lg:pt-2 lg:col-span-6'>
+					<SocialElement
+						icon={<MdLocationOn className='text-white' />}
+						name={t("address")}
+					/>
+				</div>
+			</div>
+			<div className='col-span-4'>
+				<img
+					src={photo1}
+					alt='Register Image'
+					className='w-full max-md:h-[300px] md:h-[600px] object-cover'
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default Register;
